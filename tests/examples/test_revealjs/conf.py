c22768a9f6c59ceef786cfe3130e8ccd0da3511c
Ashley Trinh <ashley@hackbrightacademy.com>
2020-08-21 11:18:17 -0700

    Clean up conf.py

--- tests/examples/test_revealjs/conf.py
+++ tests/examples/test_revealjs/conf.py
@@ -10,36 +10,7 @@ import os
 sys.path.append(os.path.abspath('./../../..'))
 
 master_doc = 'index'
-templates_path = ['_templates']
-
 extensions = [
     'sphinxlectern'
 ]
-
 revealjs_theme = 'revealjs'
-# exclude_patterns = ['_build',
-#                     'skit.rst',
-#                     '**/rubric',
-#                     '**/skit',
-#                     '**/*-demo',
-#                     'meta',
-#                     '**/*_',
-#                     '**/env',
-#                     '**/venv']
-
-# pygments_style = 'sphinx'
-
-# rst_epilog = symbols + python_substitutions + text_editor_substitutions
-
-# html_context = {'backurl': '/'}
-# html_theme = handouts_theme = 'handouts'
-# html_show_sourcelink = False
-# html_show_sphinx = False
-# html_use_index = False
-# html_domain_indices = False
-# html_scaled_image_link = False
-# html_copy_source = False
-# html_add_permalinks = ""
-# revealjs_theme = 'revealjs'
-
-# hb_hostname = 'fellowship.hackbrightacademy.com'
